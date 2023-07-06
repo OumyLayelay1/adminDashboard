488a4808d6007d49830c5a0cbf3c950e14c96700
OumyLayelay1 <kaneoumylaye@gmail.com>
2023-07-06 13:30:46 +0100

    ajout de la responsivité et le javascript

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '8286',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'LARVENDER Kf102 Drone',
+        productNumber: '6898',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'RUKO F11 Pro Drone',
+        productNumber: '1120',
+        paymentStatus: 'Paid',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Drone with Camera',
+        productNumber: '0876',
+        paymentStatus: 'Due',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'GPS 4K Drone',
+        productNumber: '4001',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    }
+]
